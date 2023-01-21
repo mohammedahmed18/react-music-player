5f7ca860070ba2462911c9b01254bff6a1d75623
mohammed ahmed <64513301+mohammedahmed18@users.noreply.github.com>
2023-01-21 21:47:52 +0200

    remove all songs

--- src/songs.utlil.js
+++ src/songs.utlil.js
@@ -1,106 +1,106 @@
 const songs = [
-  {
-    _id: "1",
-    name: "After Dark",
-    artist: "Mr.kitty",
-    isPlaying: false,
-    color: "#C76BDB",
-    image:
-      "https://i.pinimg.com/originals/f8/f5/70/f8f570fac8c9f08b21b1ae1d99b39825.jpg",
-    src: "/songs/" + "Mr.Kitty - After Dark" + ".mp3",
-  },
-  {
-    _id: "2",
-    name: "terror in japan piano",
-    artist: "lofi music",
-    isPlaying: false,
-    color: "#C76BDB",
-    image:
-      "https://pa1.narvii.com/5765/b4287f90c96d3667d3526ee552456144cf876b74_hq.gif",
-    src: "/songs/" + "terror in japan - trigger" + ".mp3",
-  },
+//   {
+//     _id: "1",
+//     name: "After Dark",
+//     artist: "Mr.kitty",
+//     isPlaying: false,
+//     color: "#C76BDB",
+//     image:
+//       "https://i.pinimg.com/originals/f8/f5/70/f8f570fac8c9f08b21b1ae1d99b39825.jpg",
+//     src: "/songs/" + "Mr.Kitty - After Dark" + ".mp3",
+//   },
+//   {
+//     _id: "2",
+//     name: "terror in japan piano",
+//     artist: "lofi music",
+//     isPlaying: false,
+//     color: "#C76BDB",
+//     image:
+//       "https://pa1.narvii.com/5765/b4287f90c96d3667d3526ee552456144cf876b74_hq.gif",
+//     src: "/songs/" + "terror in japan - trigger" + ".mp3",
+//   },
 
-  {
-    _id: "3",
-    name: "night trouble",
-    artist: "petit biscuit",
-    isPlaying: false,
-    color: "#C76BDB",
-    image:
-      "https://i.pinimg.com/originals/d3/4e/83/d34e83b148137f950136a6a1dde10c04.png",
-    src: "/songs/" + "PETIT BISCUIT - Night Trouble" + ".mp3",
-  },
-  {
-    _id: "4",
-    name: "bleach",
-    artist: "Hurtwave",
-    isPlaying: false,
-    color: "#934A69",
-    image:
-      "https://images.genius.com/9641b7cef4c2b92f995d707ae72ec7a4.300x300x1.jpg",
-    src: "/songs/" + "1 Hurtwave - Bleach" + ".mp3",
-  },
-  {
-    _id: "5",
-    name: "ís",
-    artist: "yoko kanno",
-    isPlaying: false,
-    color: "#97A5C2",
-    image:
-      "https://honeysanime.com/wp-content/uploads/2016/09/Zankyou-no-Terror-Character-TI-700x476.jpg",
-    src: "/songs/" + "ís - Yoko Kanno x POP ETC" + ".mp3",
-  },
-  {
-    _id: "6",
-    name: "von - Hope",
-    artist: "yoko kanno",
-    isPlaying: false,
-    color: "#97A5C2",
-    image:
-      "https://i.pinimg.com/originals/cc/d7/31/ccd731386531cd8f1c460263391b3e4a.jpg",
-    src: "/songs/" + "von" + ".mp3",
-  },
+//   {
+//     _id: "3",
+//     name: "night trouble",
+//     artist: "petit biscuit",
+//     isPlaying: false,
+//     color: "#C76BDB",
+//     image:
+//       "https://i.pinimg.com/originals/d3/4e/83/d34e83b148137f950136a6a1dde10c04.png",
+//     src: "/songs/" + "PETIT BISCUIT - Night Trouble" + ".mp3",
+//   },
+//   {
+//     _id: "4",
+//     name: "bleach",
+//     artist: "Hurtwave",
+//     isPlaying: false,
+//     color: "#934A69",
+//     image:
+//       "https://images.genius.com/9641b7cef4c2b92f995d707ae72ec7a4.300x300x1.jpg",
+//     src: "/songs/" + "1 Hurtwave - Bleach" + ".mp3",
+//   },
+//   {
+//     _id: "5",
+//     name: "ís",
+//     artist: "yoko kanno",
+//     isPlaying: false,
+//     color: "#97A5C2",
+//     image:
+//       "https://honeysanime.com/wp-content/uploads/2016/09/Zankyou-no-Terror-Character-TI-700x476.jpg",
+//     src: "/songs/" + "ís - Yoko Kanno x POP ETC" + ".mp3",
+//   },
+//   {
+//     _id: "6",
+//     name: "von - Hope",
+//     artist: "yoko kanno",
+//     isPlaying: false,
+//     color: "#97A5C2",
+//     image:
+//       "https://i.pinimg.com/originals/cc/d7/31/ccd731386531cd8f1c460263391b3e4a.jpg",
+//     src: "/songs/" + "von" + ".mp3",
+//   },
 
-  {
-    _id: "7",
-    name: "Black and blue",
-    artist: "Hurtwave",
-    isPlaying: false,
-    color: "#97A5C2",
-    image:
-      "https://img.wynk.in/unsafe/275x275/filters:no_upscale():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/music/1619580534/srch_universalmusic_00602438163793-GBUM72100214.jpg",
-    src: "/songs/" + "Hurtwave - Black and Blue" + ".mp3",
-  },
+//   {
+//     _id: "7",
+//     name: "Black and blue",
+//     artist: "Hurtwave",
+//     isPlaying: false,
+//     color: "#97A5C2",
+//     image:
+//       "https://img.wynk.in/unsafe/275x275/filters:no_upscale():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/music/1619580534/srch_universalmusic_00602438163793-GBUM72100214.jpg",
+//     src: "/songs/" + "Hurtwave - Black and Blue" + ".mp3",
+//   },
 
-  {
-    _id: "8",
-    name: "Memories pt.1",
-    artist: "Ruck p",
-    isPlaying: false,
-    color: "#DB6A69",
-    image:
-      "https://chillhop.com/wp-content/uploads/2021/07/034f52eeaeceb144cca67b0930d705d32fc39e37-1024x1024.jpg",
-    src: "https://mp3.chillhop.com/serve.php/?mp3=21660",
-  },
-  {
-    _id: "9",
-    name: "Roses n Flames",
-    artist: "C Y G N",
-    isPlaying: false,
-    color: "#C76BDB",
-    image:
-      "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
-    src: "https://mp3.chillhop.com/serve.php/?mp3=14985",
-  },
-  {
-    _id: "10",
-    name: "Vitamin D",
-    artist: "fantompower",
-    isPlaying: false,
-    color: "#C76BDB",
-    image:
-      "https://chillhop.com/wp-content/uploads/2021/07/3b73a510169f14c4af83ac4016e809917412702b-1024x1024.jpg",
-    src: "https://mp3.chillhop.com/serve.php/?mp3=22790",
-  },
+//   {
+//     _id: "8",
+//     name: "Memories pt.1",
+//     artist: "Ruck p",
+//     isPlaying: false,
+//     color: "#DB6A69",
+//     image:
+//       "https://chillhop.com/wp-content/uploads/2021/07/034f52eeaeceb144cca67b0930d705d32fc39e37-1024x1024.jpg",
+//     src: "https://mp3.chillhop.com/serve.php/?mp3=21660",
+//   },
+//   {
+//     _id: "9",
+//     name: "Roses n Flames",
+//     artist: "C Y G N",
+//     isPlaying: false,
+//     color: "#C76BDB",
+//     image:
+//       "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+//     src: "https://mp3.chillhop.com/serve.php/?mp3=14985",
+//   },
+//   {
+//     _id: "10",
+//     name: "Vitamin D",
+//     artist: "fantompower",
+//     isPlaying: false,
+//     color: "#C76BDB",
+//     image:
+//       "https://chillhop.com/wp-content/uploads/2021/07/3b73a510169f14c4af83ac4016e809917412702b-1024x1024.jpg",
+//     src: "https://mp3.chillhop.com/serve.php/?mp3=22790",
+//   },
 ];
 export default songs;
